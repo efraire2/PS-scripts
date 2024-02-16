06e13d9661891b26b0052075588c3d64b3c47d96
efraire2 <efraire2@gmail.com>
2024-02-16 16:11:42 -0500

    Create Distro-Bulk-Add.ps1
    ps script for distro groups bulk add

---
+++ Distro-Bulk-Add.ps1
@@ -0,0 +1,25 @@
+#input email ID of created distro email
+$GroupEmailID = "legalcenter@aspirion.com"
+
+#csv file with users emails
+$CSVFile = "C:\Temp\dlusers.csv"
+ 
+#Connect to Exchange Online
+Connect-ExchangeOnline -ShowBanner:$False
+ 
+#Get Existing Members of the Distribution List
+$DLMembers =  Get-DistributionGroupMember -Identity $GroupEmailID -ResultSize Unlimited | Select -Expand PrimarySmtpAddress
+ 
+#Import Distribution List Members from CSV
+Import-CSV $CSVFile -Header "UPN" | ForEach {
+    #Check if the Distribution List contains the particular user
+    If ($DLMembers -contains $_.UPN)
+    {
+        Write-host -f Yellow "User is already member of the Distribution List:"$_.UPN
+    }
+    Else
+    {        
+        Add-DistributionGroupMember –Identity $GroupEmailID -Member $_.UPN
+        Write-host -f Green "Added User to Distribution List:"$_.UPN
+    }
+}
